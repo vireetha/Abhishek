76403c653eee7c90f06b8055984fe8c53ae18119
vireetha <dudalaviree69@gmail.com>
2024-12-22 15:10:46 +0530

    user

---
+++ .vscode/Hello user.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Hello {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        System.out.println("The String is "+str);
+        int num = scn.nextInt();
+        System.out.println("Number is "+num);
+        float fnum = scn.nextFloat();
+        System.out.println("The floating num is "+fnum);
+    }
+}
