b7c003e972936e63a63e269f293a710f22a49195
vireetha <dudalaviree69@gmail.com>
2025-01-18 15:37:06 +0530

    Factorization

---
+++ PrimeFactorization.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class PrimeFactorization {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for (int div = 2; div * div <= n; div++) {
+            while (n % div == 0) {
+                n = n / div;
+                System.out.print(div + " ");
+            }
+        }
+        if (n != 1) {
+            System.out.print(n);
+        }
+    }
+}
